98a2a866f0d152398fe4bae3ea160b12171bca09
Unknown <floydwin7@gmail.com>
2018-02-03 22:40:18 -0800

    Added mavros_msgs/State and mavros_msgs/BatteryStatus

---
+++ mavros_msgs/BatteryStatus.cs
@@ -0,0 +1,43 @@
+﻿using SimpleJSON;
+using ROSBridgeLib.std_msgs;
+
+namespace ROSBridgeLib.mavros_msgs
+{
+    public class BatteryStatus : ROSBridgeMsg
+    {
+        // Represent battery status from SYSTEM_STATUS
+        
+        public HeaderMsg Header;
+        public float Voltage; // [V]
+        public float Current; // [A]
+        public float Remaining; // 0..1
+
+        public BatteryStatus(HeaderMsg header)
+        {
+            this.Header = header;
+        }
+
+        public BatteryStatus(JSONNode msg)
+        {
+            Header = new HeaderMsg(msg["header"]);
+            Voltage = float.Parse(msg["voltage"]);
+            Current = float.Parse(msg["current"]);
+            Remaining = float.Parse(msg["remaining"]);
+        }
+
+        public static string GetMessageType()
+        {
+            return "mavros_msgs/BatteryStatus";
+        }
+        
+        public override string ToString()
+        {
+            return $"mavros_msgs/BatteryStatus [header={Header}, voltage={Voltage}, current={Current}, remainging={Remaining}]";
+        }
+
+        public override string ToYAMLString()
+        {
+            return $"{{\"header\" : {Header}, \"voltage\" : {Voltage}, \"current\" : {Current}, \"remaining\" : {Remaining}}}";
+        }
+    }
+}
---
+++ mavros_msgs/State.cs
@@ -0,0 +1,47 @@
+﻿using SimpleJSON;
+using ROSBridgeLib.std_msgs;
+
+namespace ROSBridgeLib.mavros_msgs
+{
+    public class State : ROSBridgeMsg
+    {
+        public HeaderMsg Header;
+        public bool Connected;
+        public bool Armed;
+        public bool Guided;
+        public string Mode;
+        public uint SystemStatus;
+
+        public State(HeaderMsg header, string mode)
+        {
+            Header = header;
+            Mode = mode;
+        }
+        
+        public State(JSONNode msg)
+        {
+            Header = new HeaderMsg(msg["header"]);
+            Connected = bool.Parse(msg["connected"]);
+            Armed = bool.Parse(msg["armed"]);
+            Guided = bool.Parse(msg["guided"]);
+            Mode = msg["mode"];
+            SystemStatus = uint.Parse(msg["system_status"]);
+        }
+
+        public static string GetMessageType()
+        {
+            return "mavros_msgs/State";
+        }
+        
+        public override string ToString()
+        {
+            return $"mavros_msgs/State [header={Header}, connected={Connected}, armed={Armed}, guided={Guided}, mode={Mode}, system_status={SystemStatus}]";
+        }
+
+        public override string ToYAMLString()
+        {
+            return $"{{\"header\" : {Header}, \"connected\" : {Connected}, \"armed\" : {Armed}, \"guided\" : {Guided}, \"mode\" : {Mode}, \"system_status\" : {SystemStatus}}}";            
+        }
+        
+    }
+}
